dccc7cf4029f13bb5bfa30f3bec3f1be169ecf25
Kaai Suzuki <suzydp@me.com>
2019-01-15 10:47:34 -0800

    WIP - test drive

---
+++ actors/BaseActor.js
@@ -0,0 +1,9 @@
+// Actor denifnition class
+class BaseActor {
+  testMethod(aBunchOfText){
+    console.log(aBunchOfText);
+    console.log(typeof(aBunchOfText));
+  }
+}
+
+module.exports = BaseActor;
