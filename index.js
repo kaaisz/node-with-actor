3632c0e3e67c3f184220414505305290cdcdcb5a
Kaai Suzuki <suzydp@me.com>
2019-01-15 10:53:59 -0800

    final code for now

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-var actors = require('comedy');
-var vorpal = require('vorpal');
-var clc = require("cli-color");
+const actors = require('comedy');
+const dddd = require('comedy');
+const vorpal = require('vorpal');
+const clc = require('cli-color');
 
-var error = clc.red.bold;
-var warn = clc.yellow;
-var notice = clc.blue;
+const err = clc.red.bold;
+const warn = clc.yellow;
+const notice = clc.blue;
 
-function StartApplication()
-{
+const StartApplication = () => {
+  // invoke to start actor system (code is written below)
   StartActors();
 }
 
-function StopApplication()
-{}
+const StopApplicaiton = () => {
 
-function StartActors()
-	{
-    console.log(notice("STARTING ACTORS"));
-    actors()
-      .rootActor() // Get a root actor reference.
-      .then(rootActor =>
+}
 
-            rootActor.createChild('/actors/BaseActor') // Create a module-defined child actor.
-          )
-      .then(myActor =>
-        // Our actor is ready, we can send messages to it.
-        myActor.send('testMethod', '50'))
+const StartActors = () => {
+  //actors.rootActor is imported from comedy package
+  console.log('starting actors');
+  actors()
+    .rootActor()
+    .then(rootActor => {
+            //import baseactor
+            rootActor.createChild('/actors/BaseActor');
+          })
+    .then(myActor => {
+            //import baseactor - our actor is ready
+            myActor.send('testMethod', '50');
+          });
+    console.log('finished starting actors')
+}
 
-  console.log(notice("FINISHED STARTING ACTORS"));
-	}
+const StopActors = () => {
 
-function StopActors()
-{}
+}
 
-
-
-function ApplicationStartup()
-  {
-  console.log(notice("================================================="));
+const ApplicationStartup = () => {
+  console.log(notice("============================="));
   console.log(notice("STARTING SERVER"));
-  console.log(notice("================================================="));
-  console.log(error("Error!: Errors will appear in this color"));
-	console.log(warn("Warning: Warnings will appear in this color"));
-	console.log(notice("Notice: Will appear in this color"));
+  console.log(notice("============================="));
+  console.log(err("error! as a red one"));
+  console.log(warn("warn as a yellow one"));
+  console.log(notice("notice as a blue one"));
+
   StartActors();
-  }
-
-
+}
 
 ApplicationStartup();
